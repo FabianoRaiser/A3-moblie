f0a54709b61505e9572dd06e536e8004fca89d35
Fabiano Raiser <fabiano.raiser16@gmail.com>
2024-09-13 16:45:31 -0300

    feat: :construction: add arquivos de ambiente de desenvolvimento

---
+++ lib/src/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFE8B86D);
+const Color secundaryColor = Color(0xFFCEDF9F);
+const Color thirdColor = Color(0xFFF1F3C2);
+const Color menusColor = Color(0xFFE8B86D);
